edafff2582a03bab075abaeab0abf7a3db004c1d
Ian Schoonover <learntocodeinfo@gmail.com>
2024-02-12 22:02:18 -0600

    Change allowed hosts to all

--- static_site_optimizer/static_site_optimizer/settings.py
+++ static_site_optimizer/static_site_optimizer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://www.sso.devsprouthosting.com", "https://sso.devsprouthosting.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
