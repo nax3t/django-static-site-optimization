8335cf7d93f3f76c3af3825618e7fd0698b4c068
Ian Schoonover <learntocodeinfo@gmail.com>
2024-02-26 19:02:32 -0600

    Update STATIC_ROOT to 'static/'

--- static_site_optimizer/settings.py
+++ static_site_optimizer/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static_files/'
+STATIC_ROOT = 'static/'
 
 
 # Default primary key field type
